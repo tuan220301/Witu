d18879a5f993f725a74c2b2f8d59e684b6015a7b
2108win <dangcapcuawin@gmail.com>
2022-12-22 11:25:41 +0700

    add darkmode

--- frontend/Blog_app/Components/Buttons/Toggle_menu.tsx
+++ frontend/Blog_app/Components/Buttons/Toggle_menu.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
-import { AiOutlineHome } from 'react-icons/ai';
-import { BiAddToQueue } from 'react-icons/bi';
-import { FiSettings } from 'react-icons/fi';
-import { HiUserGroup } from 'react-icons/hi';
-import '../style.css'
+import { AiOutlineHeart, AiOutlineHome } from "react-icons/ai";
+import { BiAddToQueue, BiSearch } from "react-icons/bi";
+import { FiSettings } from "react-icons/fi";
+import { HiHome, HiUserGroup } from "react-icons/hi";
+import { RiMessengerLine } from "react-icons/ri";
+import Switcher from "../Switcher";
+import { AvaBtn } from "./Ava_Btn";
+import "../style.css";
 interface IMenuProps {
   isMenuOpen: boolean;
 }
@@ -12,36 +15,70 @@ interface IMenuProps {
 export const Menu = ({ isMenuOpen }: IMenuProps) => {
   // console.log("isMenuOpen: " + isMenuOpen)
   return (
-    <div className={`app-menu ${isMenuOpen ? "menu-open" : ""}`}>
-      <ul className="w-[100%] h-[100%]">
-        <li>
-          <img src={require('../../../assets/imgs/Logo.png')} className='w-[250px] h-[200px]' alt="logo" />
-        </li>
-        <li className="w-[100%] p-[20px] text-2xl text-black hover:bg-[#f4f4f4] hover:font-bold">
-          <div className="flex items-stretch">
-            <span className="pr-[8px]"><AiOutlineHome /></span>
-            <Link to="/" className="w-full">Home</Link>
-          </div>
-        </li>
-        {/* <li className="w-[100%] p-[20px] text-2xl text-black hover:bg-[#f4f4f4] hover:font-bold">
-          <div className="flex items-stretch">
-            <span className="pr-[8px]"><BiAddToQueue /></span>
-            <Link className="w-full" to="/add_blog" >Add Blog</Link>
-          </div>
-        </li> */}
-        <li className="w-[100%] p-[20px] text-2xl text-black hover:bg-[#f4f4f4] hover:font-bold">
-          <div className="flex items-stretch">
-            <span className="pr-[8px]"><FiSettings /></span>
-            <Link className="w-full" to="/setting" >Settings</Link>
-          </div>
-        </li>
-        <li className="w-[100%] p-[20px] text-2xl text-black hover:bg-[#f4f4f4] hover:font-bold">
-          <div className="flex items-stretch">
-            <span className="pr-[8px]"><HiUserGroup /></span>
-            <Link className="w-full" to="/about_us" >About Us</Link>
-          </div>
-        </li>
-      </ul>
+    <div className={`app-menu bg-white dark:bg-slate-900 ${isMenuOpen ? "menu-open" : ""}`}>
+      <div className="max-w-[70px] md:max-w-full min-h-screen p-[8px] flex flex-col text-gray-800 dark:text-white">
+        <Link to="/" className="block h-[200px]">
+          <img src={require("../../../assets/imgs/Logo.png")} className="" alt="logo" />
+        </Link>
+        <Link
+          to="/"
+          className="flex items-center relative w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <HiHome size={30} />
+          <span className="pl-[12px] w-full hidden md:block">Trang chủ</span>
+        </Link>
+        <Link
+          to="/search"
+          className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <BiSearch size={30} />
+          <span className="pl-[12px] w-full hidden md:block">Tìm kiếm</span>
+        </Link>
+        <Link
+          to="/posts"
+          className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <BiAddToQueue size={30} />
+          <span className="pl-[12px] w-full hidden md:block">Posts</span>
+        </Link>
+        <Link
+          to="/chat"
+          className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <RiMessengerLine size={30} />
+          <span className="pl-[12px] w-full hidden md:block">Tin nhắn</span>
+        </Link>
+        <Link
+          to="/notification"
+          className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <AiOutlineHeart size={30} />
+          <span className="pl-[12px] w-full hidden md:block">Thông báo</span>
+        </Link>
+        <Link
+          to="/settings"
+          className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <FiSettings size={30} />
+          <span className="pl-[12px] w-full hidden md:block">Settings</span>
+        </Link>
+        <Link
+          to="/settings"
+          className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full"
+        >
+          <AvaBtn
+            srcImg={`https://picsum.photos/200`}
+            className={`w-[30px] h-[30px] rounded-full`}
+          />
+          <span className="w-full pl-[12px] hidden md:block">Win Lã</span>
+        </Link>
+        <div className="flex items-center w-[100%] p-[12px] text-[20px] font-medium hover:bg-[#f4f4f4cc] rounded-full mt-auto">
+          <Switcher />
+          <span className="w-full pl-[12px] hidden md:block" id="theme">
+            Chuyển chủ đề
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./Blog_app/**/*.{html,js,ts,jsx,tsx}",
-    "./login_page.js",
-    "./App.js",
-
-  ],
+  content: ["./Blog_app/**/*.{html,js,ts,jsx,tsx}", "./login_page.js", "./App.js"],
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [
-  ],
-}
+  plugins: [],
+};
