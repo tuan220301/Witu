c7fae85eebce9f720007f7339eec9c693f031233
tuan220301 <tuannguyenwin01@gmail.com>
2023-01-08 02:30:18 +0700

    new route: settings app system

--- frontend/Blog_app/Modules/Setting_Components/app_settings_components.jsx
+++ frontend/Blog_app/Modules/Setting_Components/app_settings_components.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const SystemSettings = () => {
     return (
-        <div >
+        <div className="w-full h-full flex gap-[16px] flex-col overflow-y-scroll">
             SystemSettings
         </div>
     );
